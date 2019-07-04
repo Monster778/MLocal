804025155dee7f95903427f4eda05c5ec84f42f2
Monster8080 <43908973+Monster778@users.noreply.github.com>
2019-07-04 18:32:14 +0300

    Add files via upload

---
+++ MLocal.sh
@@ -0,0 +1,10 @@
+echo Thanks For Use This script 
+echo This script Made in Syria
+echo by Black Monster
+chmod 777 *
+pkg update
+pkg upgrade
+pkg install php
+pkg install python python2  
+mkdir /sdcard/php/
+php -S localhost:8080 -t /sdcard/php
